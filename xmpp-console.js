5e590da6e8c5330e4a303b89d05b4a6dfd892115
Carlos Rocha <carlos.rocha58@gmail.com>
2014-01-24 20:43:44 -0600

    Rewrite

--- xmpp-console.js
+++ xmpp-console.js
@@ -1,190 +1,180 @@
 (function() {
   'use strict';
-  
-  var HTTPBIND_URL = 'http://localhost:8000/xmpp-httpbind';
 
-  var parseXml = function(text) {
-    var doc = null;
-    if (window.DOMParser) {
-      var parser = new window.DOMParser();
-      doc = parser.parseFromString(text, 'text/xml');
-    } else if (window.ActiveXObject) {
-      doc = new window.ActiveXObject('MSXML2.DOMDocument');
-      doc.async = false;
-      doc.loadXML(text);
-    } else {
-      throw {
-        message: 'No DOMParser object found.'
-      };
-    }
-
-    var elem = doc.documentElement;
-    if (elem.getElementsByTagName('parsererror').length > 0) {
-      return null;
-    }
-    return elem;
-  };
+  var httpBindUrl = '/xmpp-httpbind';
 
   var formatXml = function(xml) {
-    var formatted = '';
-    var reg = /(>)(<)(\/*)/g;
-    xml = xml.replace(reg, '$1\r\n$2$3');
-    var pad = 0;
-    _.each(xml.split('\r\n'), function(node, index) {
-      var indent = 0;
-      if (node.match( /.+<\/\w[^>]*>$/ )) {
-        indent = 0;
-      } else if (node.match( /^<\/\w/ )) {
-        if (pad !== 0) {
-          pad -= 1;
-        }
-      } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
-        indent = 1;
-      } else {
-        indent = 0;
-      }
-  
-      var padding = '';
-      for (var i = 0; i < pad; i++) {
-        padding += '  ';
-      }
-  
-      formatted += padding + node + '\r\n';
-      pad += indent;
-    });
+    xml = xml.replace(/></g, '>\n<');
+    var indent = 0;
+    return _.reduce(xml.split('\n'), function(memo, node) {
+      if (node.match(/^<\/\w/)) indent -= 1;
 
-    return _.escape(formatted);
+      var padding = Array(indent + 1).join('  ');
+
+      if (node.match(/^<\w[^>]*[^\/]>.*$/)) indent += 1;
+
+      return memo + padding + node + '\n';
+    }, '');
+  };
+
+  var parseXml = function(val) {
+    var result = new window.DOMParser().parseFromString(val, 'text/xml');
+
+    if (result.getElementsByTagName('parsererror')) {
+      return false;
+    } else {
+      return result;
+    }
+  };
+
+  var connCallback = function(view) {
+    return function(status) {
+      switch (status) {
+        case Strophe.Status.CONNECTING:
+          view.updateStatus('Connecting&#x2026;');
+          break;
+        case Strophe.Status.CONNECTED:
+          view.connected(this);
+          break;
+        case Strophe.Status.AUTHENTICATING:
+          view.updateStatus('Authenticating&#x2026;');
+          break;
+        case Strophe.Status.AUTHFAIL:
+          view.updateStatus('Authentication failed', true);
+          view.enable();
+          break;
+        case Strophe.Status.DISCONNECTED:
+          view.updateStatis('Server unavailable', true);
+          view.enable();
+          break;
+      }
+    }
   };
 
   var LoginView = Backbone.View.extend({
-    el: $('#login-modal'),
+    id: 'login',
 
-    show: function() {
-      this.$el.modal('show');
-    },
-
-    filterOnEnter: function(e) {
-      if (e.keyCode !== 13) {
-        return;
-      }
-      this.login();
-    },
+    template: _.template($('#login-template').html()),
 
     events: {
-      'click #login-btn': 'login',
-      'keypress #jid': 'filterOnEnter',
-      'keypress #password': 'filterOnEnter'
+      'submit form': 'login'
     },
 
-    connListener: function(status) {
-      var statusMsg = $('#status-msg');
-      switch (status) {
-        case Strophe.Status.CONNECTING:
-          statusMsg.text('Connecting...');
-          break;
-        case Strophe.Status.CONNECTED:
-          statusMsg.text('');
-          this.$el.modal('hide');
-          this.trigger('connected', this.conn);
-          break;
-        case Strophe.Status.AUTHENTICATING:
-          statusMsg.text('Authenticating...');
-          break;
-        case Strophe.Status.AUTHFAIL:
-          statusMsg.text('Wrong username and password combination');
-          break;
-      }
+    render: function() {
+      this.$el.html(this.template());
+      return this;
     },
 
     login: function() {
-      var jid = $('#jid').val();
-      var password = $('#password').val();
+      var jid = this.$('#jid').val();
+      var password = this.$('#password').val();
 
-      this.conn = new Strophe.Connection(HTTPBIND_URL);
-      this.conn.connect(jid, password, _.bind(this.connListener, this));
+      var conn = new Strophe.Connection(httpBindUrl);
+      conn.connect(jid, password, connCallback(this));
+      this.$('fieldset').prop('disabled', true);
+
+      return false;
+    },
+
+    updateStatus: function(message, error) {
+      this.$('p').html(message).toggleClass('error', !!error);
+    },
+
+    enable: function() {
+      this.$('fieldset').prop('disabled', false);
+    },
+
+    connected: function(connection) {
+      this.trigger('connected', connection);
+    }
+  });
+
+  var ConsoleView = Backbone.View.extend({
+    template: _.template($('#console-template').html()),
+
+    events: {
+      'click #disconnect': 'disconnect',
+      'click #clear': 'clear',
+      'click #send': 'send'
+    },
+
+    initialize: function(options) {
+      this.conn = options.connection;
+
+      var incoming = _.bind(_.partial(this.appendToConsole, 'incoming'), this);
+      this.conn.xmlInput = incoming;
+
+      var outgoing = _.bind(_.partial(this.appendToConsole, 'outgoing'), this);
+      this.conn.xmlOutput = outgoing;
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+
+    appendToConsole: function(type, element) {
+      var consoleEl = this.$('pre');
+      var msg = Strophe.serialize(element);
+      var formatted = formatXml(msg);
+      var newEntry = $('<div>', { 'class': type }).text(formatted);
+      consoleEl.append(newEntry);
+      hljs.highlightBlock(newEntry.get(0));
+
+      consoleEl.scrollTop(consoleEl[0].scrollHeight);
+    },
+
+    disconnect: function() {
+    },
+
+    clear: function() {
+      this.$('pre').html('');
+    },
+
+    send: function() {
+      var val = this.$('textarea').val(),
+          message,
+          error;
+
+      if (!val) return;
+
+      if (val[0] === '<') {
+        message = parseXml(val);
+        if (message === false) error = 'XML error';
+      } else {
+        try {
+          message = eval(val);
+        } catch(e) {
+          error = e.message;
+        }
+      }
+
+      if (!message) {
+        this.$('#error-message').text(error);
+        return;
+      }
+
+      this.$('#error-message').text('');
+      this.conn.send(message);
     }
   });
 
   var AppView = Backbone.View.extend({
-    id: 'main-content',
-    consoleEl: $('#console'),
-    inputEl: $('#main-input'),
-    msgTemplate: _.template('<div class="<%= type %>"><%= msg %></div>'),
-
-    initialize: function() {
+    el: '#main',
+    
+    render: function() {
       this.loginView = new LoginView();
-      this.loginView.bind('connected', this.connected, this);
-      this.loginView.show();
+      this.$el.append(this.loginView.render().el);
+      this.listenTo(this.loginView, 'connected', this.showConsole);
+
+      return this;
     },
 
-    events: {
-      'click #send-message': 'sendMessage',
-      'click #disconnect': 'disconnect',
-      'click #clear': 'clearConsole'
-    },
-
-    connected: function(conn) {
-      this.conn = conn;
-      var incoming = _.bind(_.partial(this.addToConsole, 'incoming'), this);
-      var outgoing = _.bind(_.partial(this.addToConsole, 'outgoing'), this);
-      conn.xmlInput = incoming;
-      conn.xmlOutput = outgoing;
-    },
-
-    addToConsole: function(type, element) {
-      var msg = Strophe.serialize(element);
-      var formatted = formatXml(msg);
- 
-      var newEntry = $(this.msgTemplate({type: type, msg: formatted}));
-      this.consoleEl.append(newEntry);
-      hljs.highlightBlock(newEntry.get(0));
-
-      // Keep scrolling to bottom
-      this.consoleEl.scrollTop(this.consoleEl[0].scrollHeight);
-    },
-
-    sendMessage: function() {
-      var msg = this.inputEl.val();
-      var error = false;
-
-      if (msg[0] === '<') {
-        // assume raw XML message
-        var xml = parseXml(msg);
-        if (xml) {
-          this.conn.send(xml);
-          this.inputEl.val('');
-        } else {
-          error = true;
-        }
-      } else {
-        // else the message is javascript
-        try {
-          var builder = eval(msg);
-          this.conn.send(builder);
-          this.inputEl.val('');
-        } catch (e) {
-          console.log(e);
-          error = true;
-        }
-      }
-
-      if (error) {
-        this.inputEl.parent().parent().addClass('error');
-      } else {
-        this.inputEl.parent().parent().removeClass('error');
-      }
-    },
-
-    clearConsole: function() {
-      this.consoleEl.text('');
-    },
-
-    disconnect: function() {
-      this.conn.disconnect();
-      this.clearConsole();
-      this.loginView.show();
+    showConsole: function(connection) {
+      this.loginView.remove();
+      this.$el.append(new ConsoleView({ connection: connection }).render().el);
     }
   });
 
-  new AppView();
+  new AppView().render();
 })();
